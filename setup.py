1c715091582425464649cc1b27469e20cd0c3fb1
danny <63987490+hyunlae@users.noreply.github.com>
2023-09-15 14:58:19 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ncafe_downloader',
+    version='1.0',
+    packages=[],
+    url='https://github.com/gusfot',
+    license='free',
+    author='hyunlae',
+    author_email='gusfot@gmail.com',
+    description='ncafe_downloader'
+)
