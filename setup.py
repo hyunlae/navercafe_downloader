3f9e156eff3e49147f527dec287e7d8cb275430d
danny <danny@zent.kr>
2024-01-25 15:11:56 +0900

    feat(pip install 을 위한 패키지 변경):

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ncafe_downloader',
-    version='1.1',
+    version='1.1.1',
     packages=find_packages(),
     url='https://github.com/gusfot',
     license='free',
